e798de22d65d3ae62c11de5ef5ae5ff6226790d2
nihar <niharsingla001@gmail.com>
2024-08-10 12:57:02 +0530

    added functionality to the button(start)

--- src/components/TimerChallenge.js
+++ src/components/TimerChallenge.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
+
 export default function TimerChallenge({ title, targetTime }) {
+  const [timerExpired, setTimerExpired] = useState(false);
+  const [timerStated, setTimerStated] = useState(false);
+
+  let timer;
+
+  function handleStart() {
+    setTimerStated(true);
+
+    setTimerStated = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <botton>Start Challenge</botton>
+        <button onClick={timerStated ? "handleStop" : handleStart}>
+          {timerStated ? "Stop" : "Start"} Challenge
+        </button>
       </p>
-      <p className="">Time is Running... / Timer inavtive</p>
+      <p className={timerStated ? "active" : undefined}>
+        {timerStated ? "Time is Running..." : "Timer inavtive"}
+      </p>
     </section>
   );
 }
